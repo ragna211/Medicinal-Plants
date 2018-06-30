4b4c9d8587389ea413672038a7f0299dae506ef4
Japkeerat Singh <japkeerat21@gmail.com>
2018-06-30 21:33:11 +0530

    Removed an error in importing package

--- DataSet/collectingImages.py
+++ DataSet/collectingImages.py
@@ -1,4 +1,4 @@
-import google_images_download as gid
+from google_images_download import google_images_download as gid
 
 plants = ["Tailspatra", "Ulatkambal", "Gunja", "Kankati", "Babul", "Catechu", "Shikakai", "Gandana", "Apamarga", "Atis", "Bachnag", "Bach", "Adulsa", "Bael", "Siris", "Onions", "Garlic", "Ghrita Kumari", "Khulanjan", "Saptaparna", "Akarkara", "Kalmegh", "Suwa", "Rohitika", "Ajmod", "Samudra Palak"]
 
